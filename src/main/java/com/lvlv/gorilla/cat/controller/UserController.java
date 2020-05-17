3ea99e357c6b86e594c465cef394b5a3c2f257e4
Lvvv <11437223@qq.com>
2020-05-17 22:50:13 +0800

    2020-05-18 20:50

--- src/main/java/com/lvlv/gorilla/cat/controller/UserController.java
+++ src/main/java/com/lvlv/gorilla/cat/controller/UserController.java
@@ -1,16 +1,24 @@
 package com.lvlv.gorilla.cat.controller;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import com.lvlv.gorilla.cat.annotation.UserLoginToken;
+import com.lvlv.gorilla.cat.entity.sql.User;
+import com.lvlv.gorilla.cat.service.UserService;
+import com.lvlv.gorilla.cat.util.MysqlUtil;
+import com.lvlv.gorilla.cat.util.PasswordUtil;
 import com.lvlv.gorilla.cat.util.RestResult;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
 @Slf4j
 public class UserController {
+
+    @Autowired
+    UserService userService;
 
     @UserLoginToken
     //@GetMapping(value = "/test", produces = "application/json; charset=utf-8")
@@ -21,4 +29,50 @@ public class UserController {
         return result;
         //throw new RuntimeException("error");
     }
+
+    // 手机号、密码 登录
+    @PostMapping("/login")
+    public RestResult login(@RequestBody User user) {
+
+        RestResult result = new RestResult();
+
+        user.setUid(MysqlUtil.getNextUid());
+
+        // 获取盐
+        user.setSalt(PasswordUtil.getRandomSalt());
+
+        // 将口令加盐并加密
+        user.setPassword(PasswordUtil.getEncryptedPasswordWithSalt(user.getPassword(),user.getSalt()));
+
+        String token="";
+        token= JWT.create().withAudience(user.getUid().toString())
+                .sign(Algorithm.HMAC256(user.getPassword()));
+
+        result.setData(user);
+        result.setMessage(token);    // 使用 message 返回 token
+
+        return result;
+    }
+
+    // 手机号、短信认证码登录
+    @PostMapping("/smslogin")
+    public RestResult smsLogin(@RequestBody User user) {
+
+        return null;
+    }
+
+    // 手机号、密码、短信认证码注册
+    @PostMapping("/register")
+    public RestResult register(@RequestBody User user) {
+
+        return null;
+    }
+
+    // 手机号、密码、短信认证码重置密码
+    @PostMapping("/resetpassword")
+    public RestResult resetPassword(@RequestBody User user) {
+
+        return null;
+    }
+
 }
