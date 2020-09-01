d2fcf16fbe2be182efc50382cb49b34bb315e606
Lvvv <11437223@qq.com>
2020-09-01 22:39:37 +0800

    20200901 22:39

--- src/main/java/com/lvlv/gorilla/cat/mqtt/MQTTService.java
+++ src/main/java/com/lvlv/gorilla/cat/mqtt/MQTTService.java
@@ -36,6 +36,7 @@ public class MQTTService {
 
     /**
      * 获取 App 端监听的接收消息的 topic
+     *
      * @param uid
      * @return
      */
@@ -45,22 +46,24 @@ public class MQTTService {
 
     /**
      * 解析 入口
+     *
      * @param topic
      * @param payload
      */
     public void parseMessage(String topic, String payload) {
 
         if (MQTTClienter.TOPIC_LOGIN.equals(topic)) {
-            userLogin(topic,payload);
+            userLogin(topic, payload);
         } else if (MQTTClienter.TOPIC_LOGOUT.equals(topic)) {
-            userLogout(topic,payload);
+            userLogout(topic, payload);
         } else if (MQTTClienter.TOPIC_CAT_EAR.equals(topic)) {
-            userMessage(topic,payload);
+            userMessage(topic, payload);
         }
     }
 
     /**
      * 处理用户上线消息
+     *
      * @param topic
      * @param payload
      */
@@ -69,9 +72,10 @@ public class MQTTService {
             return;
         }
 
-        if (redisUtil.set(RedisKeyUtil.getMQTTOnlineKey(payload),"1")){
+        if (redisUtil.set(RedisKeyUtil.getMQTTOnlineKey(payload), "1")) {
             log.info("在 Redis 中设置 MQTT key 成功 uid = : " + payload);
-        };
+        }
+        ;
 
         // 发送未达的消息
         Long uid = Long.parseLong(payload);
@@ -79,7 +83,7 @@ public class MQTTService {
         for (MQTTMessage message : list) {
             try {
                 String forSend = mapper.writeValueAsString(message);
-                if (mqttClienter.publish(getAppListeningTopic(payload),forSend)) {
+                if (mqttClienter.publish(getAppListeningTopic(payload), forSend)) {
                     mqttMessageService.setMessageSended(uid);
                 }
             } catch (Exception e) {
@@ -89,6 +93,7 @@ public class MQTTService {
 
     /**
      * 处理用户下线消息
+     *
      * @param topic
      * @param payload
      */
@@ -104,6 +109,7 @@ public class MQTTService {
 
     /**
      * 处理用户消息
+     *
      * @param topic
      * @param payload
      */
@@ -111,12 +117,14 @@ public class MQTTService {
         MQTTMessage mqttMessage;
         try {
             // 解析 App 消息并保存
-            mqttMessage = mapper.readValue(payload,MQTTMessage.class);
+            mqttMessage = mapper.readValue(payload, MQTTMessage.class);
             mqttMessage.setFlagSent(1);    // 由于 App 端可能没有设置此字段, 因此在此设置为已发送
             mqttMessageService.insertMessage(mqttMessage);
 
             if ("makeFriend".equals(mqttMessage.getCommand())) {
                 makeFriend(mqttMessage);
+            } else if ("makeFriendResponse".equals(mqttMessage.getCommand())) {
+                makeFriendResponse(mqttMessage);
             } else {
                 log.warn("undefined MQTT message type!");
             }
@@ -126,21 +134,22 @@ public class MQTTService {
     }
 
     /**
-     * 交友消息处理
+     * 求加友请求 消息处理
+     *
      * @param mqttMessage
      */
     private void makeFriend(MQTTMessage mqttMessage) {
         try {
-            // 使用 客户端 payload 构建 转发 payload
+            // 使用 app payload 构建 转发 payload
             Map<String, String> payloadObj = mapper.readValue(mqttMessage.getPayload(), Map.class);
-            payloadObj.put("uid",mqttMessage.getSenderId().toString());
+            payloadObj.put("uid", mqttMessage.getSenderId().toString());
 
             // 把请求加友者的信息放进发给被请求者的 payload 中
             User user = userService.findUserByUid(mqttMessage.getSenderId());
             if (user != null) {
-                payloadObj.put("nickname",user.getNickname());
-                payloadObj.put("avatar",user.getAvatar());
-                payloadObj.put("profile",user.getProfile());
+                payloadObj.put("nickname", user.getNickname());
+                payloadObj.put("avatar", user.getAvatar());
+                payloadObj.put("profile", user.getProfile());
             }
 
             String friendId = payloadObj.get("friendId");
@@ -157,7 +166,7 @@ public class MQTTService {
             if (redisUtil.hasKey(RedisKeyUtil.getMQTTOnlineKey(friendId))) {
                 String forSend = mapper.writeValueAsString(message);
 
-                if (mqttClienter.publish(getAppListeningTopic(friendId),forSend)) {
+                if (mqttClienter.publish(getAppListeningTopic(friendId), forSend)) {
                     message.setSendTime(new Date());
                     message.setFlagSent(1);
                 }
@@ -171,4 +180,43 @@ public class MQTTService {
         }
     }
 
+    /**
+     * 求加友响应 消息处理
+     * @param mqttMessage
+     */
+    private void makeFriendResponse(MQTTMessage mqttMessage) {
+        try {
+            Map<String, String> payloadObj = mapper.readValue(mqttMessage.getPayload(), Map.class);
+            String friendId = payloadObj.get("friendId");
+            String result = payloadObj.get("result");
+
+            // 生成待转发的消息
+            MQTTMessage message = new MQTTMessage();
+            message.setType(2);  // 转发
+            message.setCommand(mqttMessage.getCommand());
+            message.setMsgId(mqttMessage.getMsgId());
+            message.setReceiverId(Long.parseLong(friendId));
+            message.setPayload(mqttMessage.getPayload());
+
+            // 检查 friend 是否在线,如果在线转发 求加友响应 消息
+            if (redisUtil.hasKey(RedisKeyUtil.getMQTTOnlineKey(friendId))) {
+                String forSend = mapper.writeValueAsString(message);
+
+                if (mqttClienter.publish(getAppListeningTopic(friendId), forSend)) {
+                    message.setSendTime(new Date());
+                    message.setFlagSent(1);
+                }
+            }
+
+            // 数据库中保存数据
+            mqttMessageService.insertMessage(message);
+
+            // 根据 result 保存 朋友关系
+            // to-do
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            log.error("Make friend error :" + e.getMessage());
+        }
+    }
 }
